1943d493450da8af06363598e2c1db20c33fca23
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2025-01-25 22:39:37 +0330

    customize my button

---
+++ GUI-Chat/src/main/java/Frontend/Components/Button.java
@@ -0,0 +1,16 @@
+package Frontend.Components;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Button extends JButton{
+
+    public Button(String text){
+        super(text);
+        setFont(new Font("Arial", Font.BOLD, 16));
+        setBackground(new Color(100, 150, 255));
+        setForeground(Color.WHITE);
+        setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+    }
+
+}
