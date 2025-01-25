a309ff2778bc825741158d8de3722fed43ff933f
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2025-01-25 22:41:33 +0330

    customize my text-field

--- GUI-Chat/src/main/java/Frontend/Components/TextField.java
+++ GUI-Chat/src/main/java/Frontend/Components/TextField.java
@@ -1,4 +1,13 @@
 package Frontend.Components;
 
-public class TextField {
+import javax.swing.*;
+import java.awt.*;
+
+public class TextField extends JTextField{
+
+    public TextField(){
+        setFont(new Font("Arial", Font.PLAIN, 16));
+        setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+    }
+
 }
