90d2a9c94618786e03e7d3c46f9a1d50794f7b33
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2025-01-25 23:50:30 +0330

    handle text-area in constructor of scroll class

--- GUI-Chat/src/main/java/Frontend/Components/Scroll.java
+++ GUI-Chat/src/main/java/Frontend/Components/Scroll.java
@@ -11,4 +11,13 @@ public class Scroll extends JScrollPane {
         setBackground(new Color(20, 20, 200));
     }
 
+    public Scroll(JTextArea textArea){
+        super(textArea);
+        setBorder(null); // Remove the border
+        setOpaque(false); // Make the background transparent
+        getViewport().setOpaque(false); // Make the viewport transparent
+        setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+    }
+
 }
