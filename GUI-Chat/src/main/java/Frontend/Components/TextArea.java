0682e04a5d3a11abd6ec094f4bad09ec49a4571c
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2025-01-25 23:51:02 +0330

    customize my text-area

--- GUI-Chat/src/main/java/Frontend/Components/TextArea.java
+++ GUI-Chat/src/main/java/Frontend/Components/TextArea.java
@@ -1,4 +1,18 @@
 package Frontend.Components;
 
-public class TextArea {
+import javax.swing.*;
+import java.awt.*;
+
+public class TextArea extends JTextArea {
+
+    public TextArea(String message){
+        super(message);
+        setFont(new Font("Arial", Font.PLAIN, 14)); // Set font size
+        setLineWrap(true);
+        setWrapStyleWord(true);
+        setEditable(false);
+        setOpaque(false);
+        setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+    }
+
 }
