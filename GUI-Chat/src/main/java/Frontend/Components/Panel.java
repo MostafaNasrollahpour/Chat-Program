ded1f7a931bc65df121e4ea7a6405a4d664ec116
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2025-01-25 22:40:27 +0330

    customize my panel

--- GUI-Chat/src/main/java/Frontend/Components/Panel.java
+++ GUI-Chat/src/main/java/Frontend/Components/Panel.java
@@ -1,4 +1,25 @@
 package Frontend.Components;
 
-public class Panel {
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class Panel extends JPanel{
+
+    public Panel(LayoutManager layoutManager){
+        setPreferredSize(new Dimension(500, 100));
+        setBackground(new Color(200, 220, 255));
+        setLayout(layoutManager);
+    }
+
+    public Panel(){
+        setBackground(new Color(0, 0, 0, 0));
+    }
+
+    public Panel(Border border){
+        setLayout(new BorderLayout());
+        setMaximumSize(new Dimension(Integer.MAX_VALUE, 50));
+        setBorder(border);
+    }
+
 }
