08074e88c18afa75fd76c8e2a260c25228a3e45e
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2025-01-26 23:25:49 +0330

    check button is it for password or not

--- GUI-Chat/src/main/java/Frontend/Components/Button.java
+++ GUI-Chat/src/main/java/Frontend/Components/Button.java
@@ -5,13 +5,24 @@ import java.awt.*;
 
 public class Button extends JButton{
 
-    public Button(String text){
+    public Button(String text, boolean pass){
         super(text);
-        setFont(new Font("Arial", Font.BOLD, 16));
-        setBackground(new Color(100, 150, 255));
-        setForeground(Color.WHITE);
-        setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
         setFocusable(false);
+
+        if(pass){
+            setBackground(new Color(9, 70, 158));
+            setForeground(Color.WHITE);
+            setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        }else{
+            setFont(new Font("Arial", Font.BOLD, 16));
+            setBackground(new Color(100, 150, 255));
+            setForeground(Color.WHITE);
+            setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        }
+    }
+
+    public Button(String text){
+        this(text, false);
     }
 
 }
