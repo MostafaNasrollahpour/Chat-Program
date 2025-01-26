b887b3e9a01f362448f96ddfe71a437bdd35bfc3
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2025-01-26 23:40:10 +0330

    add a basic structure for sign-in page

--- GUI-Chat/src/main/java/Frontend/SignIn.java
+++ GUI-Chat/src/main/java/Frontend/SignIn.java
@@ -1,4 +1,35 @@
 package Frontend;
 
+import Frontend.Components.Frame;
+import Frontend.Components.Panel;
+import Frontend.Components.PassField;
+import Frontend.Components.TextField;
+
+import java.awt.*;
+
 public class SignIn {
+
+    Frame frame;
+
+    public SignIn(Frame frame){
+        this.frame = new Frame("Sign-in", frame);
+
+        Panel panel = new Panel();
+        panel.setPreferredSize(new Dimension(400, 300));
+
+
+        TextField userName = new TextField("Your User-Name");
+        userName.setPreferredSize(new Dimension(this.frame.getWidth(), 100));
+        panel.add(userName);
+
+        PassField password = new PassField("Your Password");
+        password.setPreferredSize(new Dimension(this.frame.getWidth(), 100));
+        panel.add(password);
+
+
+        this.frame.add(panel, BorderLayout.NORTH);
+
+        this.frame.setVisible(true);
+    }
+
 }
