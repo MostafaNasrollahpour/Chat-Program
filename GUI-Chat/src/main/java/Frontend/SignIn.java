ca1141c1aba6f5c4f9eb70d84ffca5e9c23c30ff
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2025-01-27 00:19:38 +0330

    add continue button to sign-in page

--- GUI-Chat/src/main/java/Frontend/SignIn.java
+++ GUI-Chat/src/main/java/Frontend/SignIn.java
@@ -1,35 +1,41 @@
 package Frontend;
 
+import Frontend.Components.Button;
 import Frontend.Components.Frame;
+import Frontend.Components.Label;
 import Frontend.Components.Panel;
 import Frontend.Components.PassField;
 import Frontend.Components.TextField;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class SignIn {
 
     Frame frame;
 
-    public SignIn(Frame frame){
-        this.frame = new Frame("Sign-in", frame);
+    public SignIn(Frame f){
+        this.frame = new Frame("Sign-in", f);
 
         Panel panel = new Panel();
         panel.setPreferredSize(new Dimension(400, 300));
 
 
         TextField userName = new TextField("Your User-Name");
-        userName.setPreferredSize(new Dimension(this.frame.getWidth(), 100));
+        userName.setPreferredSize(new Dimension(this.frame.getWidth() - 80, 100));
         panel.add(userName);
 
         PassField password = new PassField("Your Password");
-        password.setPreferredSize(new Dimension(this.frame.getWidth(), 100));
+        password.setPreferredSize(new Dimension(this.frame.getWidth() - 80, 100));
         panel.add(password);
+
+        Button button = new Button("Continue");
+        panel.add(button);
 
 
         this.frame.add(panel, BorderLayout.NORTH);
 
+
         this.frame.setVisible(true);
     }
-
 }
