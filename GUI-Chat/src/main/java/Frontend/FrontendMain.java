bedd95199cc0dbce262a34cf444ae31262d132b9
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2025-01-26 18:56:06 +0330

    upadte run class of frontend

--- GUI-Chat/src/main/java/Frontend/FrontendMain.java
+++ GUI-Chat/src/main/java/Frontend/FrontendMain.java
@@ -3,8 +3,7 @@ package Frontend;
 public class FrontendMain {
 
     public static void main(String[] args) {
-        var x = new ChatScreen("Mostafa");
-        x.getMessage();
+        new Index();
     }
 
 }
