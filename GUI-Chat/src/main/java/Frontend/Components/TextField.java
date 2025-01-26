30381ae79a927cdb57624fea6dd3bc5172e273a8
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2025-01-26 19:24:15 +0330

    add place holder to text-filed

--- GUI-Chat/src/main/java/Frontend/Components/TextField.java
+++ GUI-Chat/src/main/java/Frontend/Components/TextField.java
@@ -5,13 +5,29 @@ import java.awt.*;
 
 public class TextField extends JTextField{
 
-    public TextField(){
+    private String placeholder;
+
+    public TextField(String placeholder){
+        this.placeholder = placeholder;
         setFont(new Font("Arial", Font.PLAIN, 16));
         setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         setBackground(new Color(9, 70, 158));
         setForeground(Color.WHITE);
         setCaretColor(Color.WHITE);
         setFont(new Font(null, Font.PLAIN, 30));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        if (getText().isEmpty()) {
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setColor(Color.GRAY); // Set placeholder color
+            g2.setFont(getFont().deriveFont(Font.ITALIC)); // Set placeholder font style
+            g2.drawString(placeholder, getInsets().left, g.getFontMetrics().getMaxAscent() + getInsets().top * 3 );
+            g2.dispose();
+        }
     }
 
 }
