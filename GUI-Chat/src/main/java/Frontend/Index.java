10e456463854dfbc4effcf8aa5f9c9e6977a0258
MostafaNasrollahpour <iammostafanasrollahpour@gmail.com>
2025-01-26 18:35:56 +0330

    add index page

---
+++ GUI-Chat/src/main/java/Frontend/Index.java
@@ -0,0 +1,37 @@
+package Frontend;
+
+import Frontend.Components.Button;
+import Frontend.Components.Frame;
+import Frontend.Components.Panel;
+
+import java.awt.*;
+
+
+public class Index {
+
+    public Index(){
+        Frame frame = new Frame("Hello");
+        Panel panel = new Panel();
+
+        panel.setBackground(new Color(100, 100, 255));
+
+        Button logInButton = new Button("Login");
+        Button signInButton = new Button("Sign in");
+
+        logInButton.addActionListener(actionEvent -> {
+            System.out.println("login");
+        });
+
+        signInButton.addActionListener(actionEvent -> {
+            System.out.println("sign in");
+        });
+
+        panel.add(logInButton);
+        panel.add(signInButton);
+
+        frame.add(panel, BorderLayout.CENTER);
+
+        frame.setVisible(true);
+    }
+
+}
